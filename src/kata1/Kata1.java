d8eb60fa4e287049ca32aa0c14526559efe4c488
alvar <alvar@DESKTOP-3SP75DF>
2023-01-21 14:05:27 +0000

    Primera versión de Kata1

---
+++ src/kata1/Kata1.java
@@ -0,0 +1,11 @@
+package kata1;
+
+import java.util.Date;
+
+public class Kata1 {
+
+    public static void main(String[] args) {
+        Person person = new Person("Alvaro", new Date(99,7,27));
+    }
+    
+}
